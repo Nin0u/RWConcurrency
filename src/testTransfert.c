ebd8e07b80ab3125dd4b1d92bdccf676d76ffd7b
Yoan ROUGEOLLE <yoyo031201@gmail.com>
2023-05-20 15:10:22 +0200

    Correction

--- src/testTransfert.c
+++ src/testTransfert.c
@@ -10,7 +10,7 @@
 
 #include "rl_library_lock.h"
 
-#define NB_PROC 10
+#define NB_PROC 100
 #define LEN_TAB 10
 #define NB_TRANS 1
 
@@ -21,7 +21,10 @@ void transfert(rl_descriptor desc, int cred, int deb, long somme)
     f1.l_type = F_WRLCK;
     f1.l_whence = SEEK_CUR;
     f1.l_len = sizeof(long);
-    rl_fcntl(desc, F_SETLKW, &f1);
+    if(rl_fcntl(desc, F_SETLKW, &f1) == -1) {
+        perror("rl_fcntl");
+        return;
+    }
 
     struct flock f2;
     f2.l_start = deb * sizeof(long);
@@ -29,7 +32,12 @@ void transfert(rl_descriptor desc, int cred, int deb, long somme)
     f2.l_whence = SEEK_CUR;
     f2.l_len = sizeof(long);
 
-    rl_fcntl(desc, F_SETLKW, &f2);
+    if(rl_fcntl(desc, F_SETLKW, &f2) == -1) {
+        f1.l_type = F_UNLCK;
+        rl_fcntl(desc, F_SETLK, &f1);
+        perror("rl_fcntl");
+        return;
+    }
 
     long *soldes = mmap(0, LEN_TAB * sizeof(long), PROT_READ | PROT_WRITE, MAP_SHARED, desc.d, 0);
     if(soldes == MAP_FAILED) {
@@ -88,8 +96,8 @@ int main()
     for(int i = 0; i < NB_TRANS; i++) {
         int deb = rand() % LEN_TAB;
         int cred = rand() % LEN_TAB;
-        cred = 1;
-        deb = 6;
+        //cred = 1;
+        //deb = 6;
         long somme = (rand() % (max - min + 1)) + min;
         transfert(desc, cred, deb, somme);
     }
